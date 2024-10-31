67ec3eedc03b59b360efcc84d961d2d24526f4b4
Quin <huangyukunmail@163.com>
2024-10-31 17:43:13 +0800

    add cmake SuperBuild script

---
+++ SuperBuild/External_EvoSeg.cmake
@@ -0,0 +1,58 @@
+set(proj python-EvoSeg)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES
+  python
+  python-ensurepip
+  python-numpy
+  python-pip
+  python-setuptools
+  python-wheel
+  )
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS scipy)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+  set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+  file(WRITE ${requirements_file} [===[
+  # [EvoSeg]
+  # Hashes correspond to the following packages:
+  fire==0.6.0
+  SimpleITK==2.3.1
+  torch==2.4.0+cu118
+  torchvision==0.19.0+cu118
+  nnunetv2==2.5.1
+  # [/EvoSeg]
+  ]===])
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+
