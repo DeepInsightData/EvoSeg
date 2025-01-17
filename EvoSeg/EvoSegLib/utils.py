fix diameter calculation

EvoSeg/EvoSegLib/utils.py
@@ -1,8 +1,12 @@
 import slicer
+import SegmentStatistics
+import numpy as np
 
-def max_diameter_of_segmentation(segmentation_node : slicer.vtkMRMLSegmentationNode, segment_id : str):
-    segment = segmentation_node.GetSegmentation().GetSegment(segment_id)
-    bounds = [0] * 6
-    segment.GetBounds(bounds)
-    max_diameter = max(bounds[1] - bounds[0], bounds[3] - bounds[2], bounds[5] - bounds[4])
-    return max_diameter
+def obbDiameterMm(segmentationNode : slicer.vtkMRMLSegmentationNode, segmentId : str):
+    segStatLogic = SegmentStatistics.SegmentStatisticsLogic()
+    segStatLogic.getParameterNode().SetParameter("Segmentation", segmentationNode.GetID())
+    segStatLogic.getParameterNode().SetParameter("LabelmapSegmentStatisticsPlugin.obb_diameter_mm.enabled",str(True))
+    segStatLogic.computeStatistics()
+    stats = segStatLogic.getStatistics()
+    obb_diameter_mm = np.array(stats[segmentId,"LabelmapSegmentStatisticsPlugin.obb_diameter_mm"])
+    return obb_diameter_mm